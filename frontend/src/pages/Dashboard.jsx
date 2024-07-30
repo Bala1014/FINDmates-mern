39deb53191c92387b40b0dd496dea32266cacbbe
Bala1014 <dhakabaladitya@gmail.com>
2024-07-30 19:56:09 +0530

    stated dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,58 @@
+import  { useState } from 'react'
+import TinderCard from 'react-tinder-card'
+
 const Dashboard = ()=>{
 
-    return(
-        <>
-            <div>dashboard</div>
-            <input type="text" placeholder="hi" />
+    const characters = [{
+        name: 'Richard Hendricks',
+        url: './img/richard.jpg'
+      },
+      {
+        name: 'Erlich Bachman',
+        url: './img/erlich.jpg'
+      },
+      {
+        name: 'Monica Hall',
+        url: './img/monica.jpg'
+      },
+      {
+        name: 'Jared Dunn',
+        url: './img/jared.jpg'
+      },
+      {
+        name: 'Dinesh Chugtai',
+        url: './img/dinesh.jpg'
+      }
+  ]
 
-        </>
+
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
+    return(
+        <div className="dashboard">
+            {/* <ChatContainer/> */}
+            <div className="swipe-container">
+                <div className='cardContainer'>
+                    {characters.map((character) =>
+                    <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                        <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                        <h3>{character.name}</h3>
+                        </div>
+                    </TinderCard>
+                    )}
+                </div>
+                {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+            </div>
+        </div>
     )
 }
 
