4286d9ba8c8a147b32cc19f502e8c2d06cf9e8d3
Bala1014 <dhakabaladitya@gmail.com>
2024-08-10 05:56:42 +0530

    cookie

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,61 @@
 const express = require('express')
 const {MongoClient} = require('mongodb') 
+const {v4 : uuidv4} = require('uuid')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
 const uri = 'mongodb+srv://baladityadhaka:NGHIqoXCQYtgRVFc@cluster49.1nojsnd.mongodb.net/FINDmates'
 const app = express();
-const port = 8000;
+const port = 8001;
+
+app.use(cors());
+app.use(express.json())
 
 app.get('/', (req,res)=>{
+    res.send("get /")
 
 })
 
 
-app.post('/signup', (req,res)=>{
-    
+app.post('/signup',async (req,res)=>{
+    const client = new MongoClient(uri);
+
+    const {email, password} = req.body;
+
+    const generatedUserId = uuidv4();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try{
+        await client.connect()
+        const database = client.db('app-data');
+        const users = database.collection('users');
+
+        const existingUser = await users.findOne({email});
+
+        if(existingUser){
+            return res.status(409).send('already exists please login');
+        }
+
+        const sanitizedEmail = email.toLowerCase();
+        const data = {
+            user_id : generatedUserId,
+            email: sanitizedEmail,
+            hashed_password : hashedPassword
+        }
+
+        const insertedUser = await users.insertOne(data);
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn : 60 * 24
+        })
+
+        res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail});
+
+
+    }catch(err){
+        console.log(err);
+    }
 })
 
 
@@ -35,4 +80,7 @@ app.get('/users', async(req,res)=>{
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
 })
+
+
+// 3.07
 
--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
+import {useCookies} from 'react-cookie';
 
 const AuthModal = ({setShowModal, signUp, setSignUp})=>{
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
     const [confirmPassword, setConfirmPassword] = useState(null);
     const [error, setError] = useState(null);
-    
+    const [cookies, setCookie, removeCookie] = useCookies(null);
 
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
+
+    let navigate = useNavigate();
 
 
     
@@ -18,13 +20,32 @@ const AuthModal = ({setShowModal, signUp, setSignUp})=>{
         setShowModal(false);
     }
 
-    const handleSubmit = (e)=>{
+
+
+    const handleSubmit = async (e)=>{
         
         e.preventDefault();
         try{
             if(signUp && (password !== confirmPassword)){
                 setError("U FKIN CUNT!!! passwords do not match ")
+                return
             }
+
+            const response = await axios.post('http://localhost:8001/signup', {email, password});
+            
+            setCookie('Email' , response.data.email)
+            setCookie('UserId' , response.data.userId);
+            setCookie('authToken', response.data.token)
+
+
+            // console.log(response);
+
+            const success = response.status === 201;
+
+            if(success){
+                navigate('/onboarding')
+            }
+
         }catch(error){
             console.log(error);
         }
